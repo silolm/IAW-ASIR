ec109a85e475cb9a78ae694c4d8fca09c8d6336a
Mario Silla Olmos <Mario Silla>
2021-09-22 18:26:58 +0200

    -a

--- PHP1/p1_8.php
+++ PHP1/p1_8.php
@@ -6,36 +6,47 @@
 </head>
 
 <body>
-<?php
 
-$frenos = 2;
+<table class="articulos">
+<tr><th colspan="2" align="center">Precio de los artículos</th></tr>
+<tr><td>Frenos</td><td>100</td></tr>
+<tr><td>Aceite</td><td>10</td></tr>
+<tr><td>Ruedas</td><td>4</td></tr>
+</table>
+
+<br><br>
+
+<?php
+$frenos = 45;
 $aceite = 1;
 $ruedas = 1;
-$errormsg = "Seleccione una unidad de cada articulo";
-
-echo "N.º frenos = " . ($frenos = $frenos + 1) . "<br>";
-echo "N.º aceite = " . "<br>";
-echo "N.º ruedas = ";
-
-
-if ( $frenos < 1 | $aceite < 1 | $ruedas < 1 ) { ?>
-<h4>Compra todo</h4>            
-<?php }else { 
-	$frenos = $frenos * 100;
-	$aceite = $aceite * 10;
-	$ruedas = $ruedas * 4;
-
-	echo "<br> Polla = " . $frenos;
-}
+$end = false;
 ?>
 
+<table class="pedido">
+<tr><th colspan="2" align="center">Pedido</th></tr>
+<tr><th>Descripción</th><th>Cantidad</th></tr>
+<tr><td>Frenos</td><td align="right"><?php echo $frenos; ?></td></tr>
+<tr><td>Aceite</td><td align="right"><?php echo $aceite; ?></td></tr>
+<tr><td>Ruedas</td><td align="right"><?php echo $ruedas; ?></td></tr>
+</table>
 
-
-
-
-
-
-
+<?php
+if ( $frenos < 1 | $aceite < 1 | $ruedas < 1 ) { 
+    $end = true;
+    ?>
+<h4>¡La petición ha de contener todos los tipos de artículos!</h4>            
+<?php }else { 
+	$tFrenos = $frenos * 100;
+	$tAceite = $aceite * 10;
+	$tRuedas = $ruedas * 4;
+	}
+    if (!$end) {
+        echo "<br><b> Cantidad total de artículos: " . $frenos + $aceite + $ruedas;
+        echo "<br> Subtotal: " . number_format($tFrenos + $tAceite + $tRuedas) . "€";
+        echo "<br> Total (I.V.A. incluido): " . number_format(($tFrenos + $tAceite + $tRuedas) * 1.21, 2) . "€ </b>";    
+    }
+?>
 
 </body>
 </html>
