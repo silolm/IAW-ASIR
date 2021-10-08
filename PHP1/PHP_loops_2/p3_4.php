af7dd076a6e6ad6f62a12443cea6aac9d9552809
Mario Silla <silolm.mail@gmail.com>
2021-10-08 13:03:50 +0200

    ejercicio 4

--- PHP1/PHP_loops_2/p3_4.php
+++ PHP1/PHP_loops_2/p3_4.php
@@ -1,11 +1,10 @@
 <?php
 for ($i = 0; $i < 24; $i++) {
+    if ($i==1) echo "<br>Fracciones de la $i:<br>";
+    else echo "<br>Fracciones de las $i:<br>";
+    
     for ($j = 0; $j <= 55; $j += 5) {
-        if ($i < 10 && $j <= 5) printf("0%s:0%s", $i, $j);
-        elseif ($i < 10 && $j > 5) printf("0%s:%s", $i, $j);
-        elseif ($i >= 10 && $j <= 5) printf("%s:0%s", $i, $j);
-        elseif ($i >= 10 && $j > 5) printf("%s:%s", $i, $j);
-
-        echo "<br>";
+        $salida=sprintf("%02d:%02d",$i,$j) ;
+        echo $salida . '<br>';
     }
 }
